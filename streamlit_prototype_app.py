89c22c92f375d1a8c0527515990f89b64fc6a754
AlphaNextE <112459605+AlphaNextE@users.noreply.github.com>
2023-01-01 13:01:16 +0900

    Update streamlit_prototype_app.py

--- streamlit_prototype_app.py
+++ streamlit_prototype_app.py
@@ -6,9 +6,6 @@ run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+_, frame = camera.read()
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+FRAME_WINDOW.image(frame)
