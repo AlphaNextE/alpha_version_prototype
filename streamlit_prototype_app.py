38b3cba4caba480e236c7e62f805a044e579dd6d
AlphaNextE <112459605+AlphaNextE@users.noreply.github.com>
2023-01-01 16:47:52 +0900

    Update streamlit_prototype_app.py

--- streamlit_prototype_app.py
+++ streamlit_prototype_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 
-img_file_buffer = st.camera_input("Take a picture")
+picture = st.camera_input("Take a picture")
 
-if img_file_buffer is not None:
-    # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
+if picture:
+    st.image(picture)
