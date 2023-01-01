b2e038e064ea4fb6ee5d128d3ef274eaa5f3d942
AlphaNextE <112459605+AlphaNextE@users.noreply.github.com>
2023-01-01 13:07:06 +0900

    Update streamlit_prototype_app.py

--- streamlit_prototype_app.py
+++ streamlit_prototype_app.py
@@ -6,6 +6,3 @@ run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
-_, frame = camera.read()
-frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-FRAME_WINDOW.image(frame)
