b9b72a52f065f50dde3b004fb5d4f615041a5654
AlphaNextE <112459605+AlphaNextE@users.noreply.github.com>
2023-01-01 16:43:15 +0900

    Update streamlit_prototype_app.py

--- streamlit_prototype_app.py
+++ streamlit_prototype_app.py
@@ -4,15 +4,17 @@ import numpy as np
 
 img_file_buffer = st.camera_input("Take a picture")
 
-# if img_file_buffer is not None:
-#     # To read image file buffer with OpenCV:
-#     bytes_data = img_file_buffer.getvalue()
-#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+st.save("img_file_buffer")
 
-#     # Check the type of cv2_img:
-#     # Should output: <class 'numpy.ndarray'>
-#     st.write(type(cv2_img))
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-#     # Check the shape of cv2_img:
-#     # Should output shape: (height, width, channels)
-#     st.write(cv2_img.shape)
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(cv2_img))
+
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
